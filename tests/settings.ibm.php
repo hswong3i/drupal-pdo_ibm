e3e639425019131d594288e8a4ec7567517accef
Wong Hoi Sing Edison <hswong3i@gmail.com>
2012-07-30 16:17:05 +0800

    * Add settings for DB2.

---
+++ tests/settings.ibm.php
@@ -0,0 +1,18 @@
+<?php
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'AL32UTF8',
+      'username' => 'db2inst1',
+      'password' => 'CHANGE',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'ibm',
+      // 30 characters prefix.
+      'prefix' => 'simpletest12345678901234567890',
+    ),
+  ),
+);
